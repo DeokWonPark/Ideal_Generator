9f94c5d994e4c70839d7d2780fe022ff71a4447f
20150477 <ejrdnjs96@gmail.com>
2020-04-07 01:59:36 +0900

    0407_DEV

--- 창설/back/index.js
+++ 창설/back/index.js
@@ -10,11 +10,6 @@ var expressErrorHandler=require("express-error-handler");
 var cookieparser=require("cookie-parser");
 var session=require("express-session");
 
-var mongo=require('mongodb').MongoClient;
-var mongoose=require('mongoose');
-
-var crypto = require('crypto');
-
 var app=express();
 
 app.set('port',process.env.PORT || 3000);
@@ -32,136 +27,84 @@ app.use(session({
     saveUninitialized:true,
 }));
 
-var database;
-var userSchema;
-var userModel;
 
 //데이터베이스 연결
-//
-function connectiondb(){
-    var databaseURL='mongodb://localhost:27017/local';
 
-    console.log("데이터베이스 연결을 시도합니다.");
-    mongoose.Promise=global.Promise;
-    mongoose.connect(databaseURL);
-    database=mongoose.connection;
+var mysql=require('mysql');
+var pool=mysql.createPool({
+    connectionLimit:10,
+    host:'localhost',
+    user:'root',
+    password:'111111',
+    database:'img_data',
+    debug:false,
+});
 
-    database.on('error',console.error.bind(console,'mongoose connection error'));
-    database.on('open',function(){
-        console.log("데이터베이스에 연결되었습니다. "+databaseURL);
-        createUserSchema();
-    });
-    database.on('disconnected',function(){
-        console.log('연결이 끊어졌습니다. 5초후에 다시 연결합니다.');
-        setInterval(connectiondb,5000);
-    });
-}
-
- // 데이터베이스 스키마 정의 및 모델생성(비밀번호 암호화)
- //
-function createUserSchema(){
-
-    userSchema=mongoose.Schema({
-        id:{type:String,required:true,unique:true},
-        hashed_password:{type:String,required:true,unique:true},
-        salt:{type:String,required:true},
-        name:{type:String,index:'hashed'},
-        age:{type:Number,'default':-1},
-        created_at:{type:Date,index:{unique:false},'default':Date.now},
-        updated_at:{type:Date,index:{unique:false},'default':Date.now},
-    });
-
-    userSchema.virtual('password').set(function(password){
-        this._password=password;
-        this.salt=this.makeSalt();
-        this.hashed_password=this.encryptPassword(password);
-    })
-    .get(function(){
-        return this._password;
-    });
-
-    userSchema.method('encryptPassword',function(plainText,inSalt){
-        if(inSalt){
-            return crypto.createHmac('sha1',inSalt).update(plainText).digest('hex');
-        }
-        else{
-            return crypto.createHmac('sha1',this.salt).update(plainText).digest('hex');
-        }
-    });
-
-    userSchema.method('makeSalt',function(){
-        return Math.round((new Date().valueOf() * Math.random())) + '';
-    });
-
-    userSchema.method('authenticate',function(plainText,inSalt,hashed_password){
-        if(inSalt){
-            return this.encryptPassword(plainText,inSalt) === hashed_password;
-        }
-        else{
-            return this.encryptPassword(plainText) === hashed_password;
-        }
-    });
-
-    userSchema.static('findById',function(id,callback){
-        return this.find({id:id},callback);
-    });
-
-    userModel=mongoose.model('users5',userSchema);
-}
 
 // 데이터베이스에 사용자 추가 
-//
-function addUser(database,id,password,name,callback){
-    var user=new userModel({'id':id,'password':password,'name':name});
-    user.save(function(err){
+
+function addUser(id,name,password,nickname,gender,callback){
+
+    pool.getConnection(function(err,conn){
         if(err){
+            if(conn){
+                conn.release();
+            }
             callback(err,null);
             return;
         }
+        var data={id:id,password:password,name:name,nickname:nickname,gender:gender};
 
-        console.log("사용자 데이터 추가함!!");
-        callback(null,user);
-    });
+        var exec=conn.query('insert into users set ?',data,function(err,result){
+            conn.release();
+
+            if(err){
+                console.log("쿼리시에 오류발생");
+                console.dir(err);
+                callback(err,null);
+            }
+            callback(null,result);
+        });
+    })
+   
 }
 
 //로그인시 사용자 인증과정
-//
-function authUser(database,id,password,callback){
 
-    userModel.findById(id,function(err,result){
+function authUser(id,password,callback){
+    pool.getConnection(function(err,conn){
         if(err){
+            if(conn){
+                conn.release();
+
+            }
             callback(err,null);
             return;
         }
-        if(result.length>0){
-            var user=new userModel({'id':id});
-            var auth=user.authenticate(password,
-                result[0]._doc.salt,result[0]._doc.hashed_password);
-            if(auth){
-                console.log("비밀번호가 일치함");
-                callback(null,result);
+        
+        var colums=['id','name','nickname'];
+        var tablename='users';
+
+        var exec=conn.query('select ?? from ?? where id=? and password=?'
+        ,[colums,tablename,id,password],function(err,rows){
+            conn.release();
+
+            if(rows.length>0){
+                console.log('일치하는 사용자를 찾음');
+                callback(null,rows);
             }
             else{
-                console.log("일치하는 비밀번호가 없습니다.");
+                console.log('일치하는 사용자를 찾지못함');
                 callback(null,null);
             }
-        }
-        else{
-            console.log("아이디 혹은 비밀번호 일치하는 사용자가 존재하지 않습니다.");
-            callback(null,null);
-        }
-    });
+        });
+    })
 }
 
 var router=express.Router();
 
-// main page 라우팅
-// router.route('/process/Worldcup').post(function(req,res){
-
-// })
-
 // 로그인 페이지 라우팅
-//
+
 router.route('/process/login').post(function(req,res){
 
     var pid=req.query.id || req.body.id;
@@ -175,28 +118,24 @@ router.route('/process/login').post(function(req,res){
         res.end();
     }
     else{
-        if(database){
-            authUser(database,pid,ppw,function(err,docs){
-                if(err) throw err;
-
-                if(docs){
-                    req.session.user={
-                        id:pid,
-                        authorized:true,
-                    }
-                    var name=docs[0].name;
+        if(pool){
+            authUser(pid,ppw,function(err,rows){
+                if(err){
+                    console.error('사용자 로그인 중 오류발생'+err.stack);
                     res.writeHead(200,{'Content-Type':'text/html ; charset=utf-8'});
-                    res.write("<h1>로그인 성공</h1>");
-                    res.write("<div><p>사용자 이름 : "+name+"</p></div>");
-                    res.write("<div><p>사용자 이름 : "+docs[0].id+"</p></div>");
-                    res.write("<br><br><a href='/process/logout' >로그아웃</a>");
+                    res.write('<h2>사용자 로그인 중 오류발생</h2>');
+                    res.write("<p>"+err.stack+"</p>");
                     res.end();
                 }
-                else{
+                if(rows){
+                    console.dir(rows);
+    
+                    var username=rows[0].name;
                     res.writeHead(200,{'Content-Type':'text/html ; charset=utf-8'});
-                    res.write("<h1>로그인 실패</h1>");
-                    res.write("<br><br><a href='/login.html' >다시 로그인 하기</a>");
-                    res.end();
+                        res.write("<h1>로그인 성공</h1>");
+                        res.write("<div><p>사용자 이름 : "+username+"</p></div>");
+                        res.write("<div><p>사용자 아이디 : "+pid+"</p></div>");
+                        res.end();
                 }
             })
         }
@@ -210,24 +149,48 @@ router.route('/process/login').post(function(req,res){
 })
 
 // 회원가입 페이지 라우팅
-//
+
 router.route('/process/adduser').post(function(req,res){
     var pid=req.query.id || req.body.id;
     var ppw=req.body.password || req.query.password;
     var pname=req.body.name || req.query.name;
+    var pnickname=req.body.nickname || req.query.nickname;
+    var pman=req.body.man || req.query.man;
+    var gender="";
+    
+    if(pman==="option1")
+        gender="남자";
+    else
+        gender="여자";
 
-    if(database){
-        addUser(database,pid,ppw,pname,function(err,result){
+    if(pool){
+        addUser(pid,pname,ppw,pnickname,gender,function(err,result){
             if(err){
-                throw err;
+                console.error('사용자 추가도중 오류발생'+err.stack);
+
+                res.writeHead(200,{'Content-Type':'text/html ; charset=utf-8'});
+                res.write('<h2>사용자 추가중 오류발생</h2>');
+                res.write("<p>"+err.stack+"</p>");
+                res.end();
+
+                return;
             }
 
-            console.dir(result);
-            res.writeHead(200,{'Content-Type':'text/html ; charset=utf-8'});
-            res.write('<h2>사용자 추가 성공</h2>');
-            res.write("<br><br><a href='/login.html'>로그인 하러가기! </a>");
-            res.end(); 
-        });
+            if(result){
+                console.dir(result);
+
+                var insertid=result.insertId;
+                console.log("추가한 레코드의 아이디 :"+insertid);
+                res.writeHead(200,{'Content-Type':'text/html ; charset=utf-8'});
+                res.write('<h2>사용자 추가성공</h2>');
+                res.end();
+            }
+            else{
+                res.writeHead(200,{'Content-Type':'text/html ; charset=utf-8'});
+                res.write('<h2>사용자 추가실패</h2>');
+                res.end();
+            }
+        })
     }
     else{
         res.writeHead(200,{'Content-Type':'text/html ; charset=utf-8'});
@@ -238,7 +201,7 @@ router.route('/process/adduser').post(function(req,res){
 })
 
 // 로그아웃 페이지 라우팅
-//
+
 router.route('/process/logout').get(function(req,res){
     if(req.session.user){
         console.log("로그아웃 합니다");
@@ -266,5 +229,4 @@ app.use(errorhandler);
 
 http.createServer(app).listen(app.get('port'),function(){
     console.log("server RUN : "+app.get('port'));
-    connectiondb();
 });
--- 창설/front/js/ui.js
+++ 창설/front/js/ui.js
@@ -1,147 +1,135 @@
 $(document).ready(function(){
 
-    var count=32;
+    var count=8;
     var count_v=count;
     var left_page="";
     var right_page="";
 
     // 첫 Start버튼 클릭 시
     $("#start").on("click",function(){
-
         $("#menu1 .box").empty();
         $("#menu1 .box").append("<div class='select'></div>");
-        $("#menu1 .select").append("<div class='select_page'><img id='girl' src='../images/girl.PNG' alt='girl'><h4 id='left_text'>여자 편</h4><div>");
-        $("#menu1 .select").append("<div class='select_page'><img id='man' src='../images/man.PNG' alt='man'><h4 id='right_text'>남자 편</h4><div>");
+        $("#menu1 .select").append("<div class='select_page'><img id='girl' src='../images/girl/girl.PNG' alt='girl'><h4 id='left_text'>여자 편</h4><div>");
+        $("#menu1 .select").append("<div class='select_page'><img id='man' src='../images/man/man.PNG' alt='man'><h4 id='right_text'>남자 편</h4><div>");
     });
 
     //여자 편 시작
     $(document).on("click","#girl",function(){
         $("#menu1 .select").prepend("<div class='select_head'><h1>"+count+"강</h1></div>");
-        $("#girl").attr("src","../images/girl1.PNG");
-        $("#girl").attr("id","irene");
+        $("#girl").attr("src","../images/girl/girl1.PNG");
+        $("#girl").attr("id","left_girl");
         $(".select_page #left_text").text("아이린");
-        left_page='#irene';
-        $("#man").attr("src","../images/girl2.PNG");
-        $("#man").attr("id","suzy");
+        left_page='irene';
+        $("#man").attr("src","../images/girl/girl2.PNG");
+        $("#man").attr("id","right_girl");
         $(".select_page #right_text").text("수지");
-        right_page="#suzy";
+        right_page="suzy";
+    });
 
-        // 내부
-        // $(document).on("change",left_page,function(){
-            
-        // })
-        $(document).on("click",left_page,function(){
-            $(left_page).animate({
-                width: "100%",
-                height: "100%",
-                opacity: 0.5
+    //여자 편
+    $(document).on("click","#left_girl",function(){
+        $("#left_girl").animate({
+            width: "100%",
+            height: "100%",
+            opacity: 0.5
+          },
+          1000,function() {
+            $("#left_girl").animate({
+                width: "300px",
+                height: "400px",
+                opacity: 1.0
               },
-              1000,function() {
-                $(left_page).animate({
-                    width: "300px",
-                    height: "400px",
-                    opacity: 1.0
-                  },
-                  0);
+              0);
 
-                $(left_page).attr("src","../images/girl3.PNG");
-                $(left_page).attr("id","Kyungri");
-                $(".select_page #left_text").text("경리");
-                left_page='#Kyungri';
-                $(right_page).attr("src","../images/girl4.PNG");
-                $(right_page).attr("id","momo");
-                $(".select_page #right_text").text("모모");
-                right_page="#momo";
-            });
+            $("#left_girl").attr("src","../images/girl/girl3.PNG");
+            $(".select_page #left_text").text("경리");
+            left_page='Kyungri';
+            $("#right_girl").attr("src","../images/girl/girl4.PNG");
+            $(".select_page #right_text").text("모모");
+            right_page="momo";
         });
+    });
 
-        $(document).on("click",right_page,function(){
-            $(right_page).animate({
-                width: "100%",
-                height: "100%",
-                opacity: 0.5
+    $(document).on("click","#right_girl",function(){
+        $("#right_girl").animate({
+            width: "100%",
+            height: "100%",
+            opacity: 0.5
+          },
+          1000,function() {
+            $("#right_girl").animate({
+                width: "300px",
+                height: "400px",
+                opacity: 1.0
               },
-              1000,function() {
-                $(right_page).animate({
-                    width: "300px",
-                    height: "400px",
-                    opacity: 1.0
-                  },
-                  0);
+              0);
 
-                $(left_page).attr("src","../images/girl3.PNG");
-                $(left_page).attr("id","Kyungri");
-                $(".select_page #left_text").text("경리");
-                left_page='#Kyungri';
-                $(right_page).attr("src","../images/girl4.PNG");
-                $(right_page).attr("id","momo");
-                $(".select_page #right_text").text("모모");
-                right_page="#momo";
-            });
+            $("#left_girl").attr("src","../images/girl/girl3.PNG");
+            $(".select_page #left_text").text("경리");
+            left_page='Kyungri';
+            $("#right_girl").attr("src","../images/girl/girl4.PNG");
+            $(".select_page #right_text").text("모모");
+            right_page="momo";
         });
     });
 
     // 남자 편 시작
     $(document).on("click","#man",function(){
         $("#menu1 .select").prepend("<div class='select_head'><h1>"+count+"강</h1></div>");
-        $("#girl").attr("src","../images/man1.PNG");
-        $("#girl").attr("id","bokumpark");
+        $("#girl").attr("src","../images/man/man1.PNG");
+        $("#girl").attr("id","left_man");
         $(".select_page #left_text").text("박보검");
-        left_page="#bokumpark";
-        $("#man").attr("src","../images/man2.PNG");
-        $("#man").attr("id","dongsukma");
+        left_page="bokumpark";
+        $("#man").attr("src","../images/man/man2.PNG");
+        $("#man").attr("id","right_man");
         $(".select_page #right_text").text("마동석");
-        right_page="#dongsukma";
+        right_page="dongsukma";
+    });
 
-        // 내부
-        $(document).on("click",left_page,function(){
-            $(left_page).animate({
-                width: "100%",
-                height: "100%",
-                opacity: 0.5
+    //남자 편
+    $(document).on("click","#left_man",function(){
+        $("#left_man").animate({
+            width: "100%",
+            height: "100%",
+            opacity: 0.5
+          },
+          1000,function() {
+            $("#left_man").animate({
+                width: "300px",
+                height: "400px",
+                opacity: 1.0
               },
-              1000,function() {
-                $(left_page).animate({
-                    width: "300px",
-                    height: "400px",
-                    opacity: 1.0
-                  },
-                  0);
+              0);
 
-                $(left_page).attr("src","../images/man3.PNG");
-                $(left_page).attr("id","bohyunahn");
-                $(".select_page #left_text").text("안보현");
-                left_page='#bohyunahn';
-                $(right_page).attr("src","../images/man4.PNG");
-                $(right_page).attr("id","GD");
-                $(".select_page #right_text").text("GD");
-                right_page="#GD";
-            });
+            $("#left_man").attr("src","../images/man/man3.PNG");
+            $(".select_page #left_text").text("안보현");
+            left_page='bohyunahn';
+            $("#right_man").attr("src","../images/man/man4.PNG");
+            $(".select_page #right_text").text("GD");
+            right_page="GD";
         });
+    });
 
-        $(document).on("click",right_page,function(){
-            $(right_page).animate({
-                width: "100%",
-                height: "100%",
-                opacity: 0.5
+    $(document).on("click","#right_man",function(){
+        $("#right_man").animate({
+            width: "100%",
+            height: "100%",
+            opacity: 0.5
+          },
+          1000,function() {
+            $("#right_man").animate({
+                width: "300px",
+                height: "400px",
+                opacity: 1.0
               },
-              1000,function() {
-                $(right_page).animate({
-                    width: "300px",
-                    height: "400px",
-                    opacity: 1.0
-                  },
-                  0);
+              0);
 
-                $(left_page).attr("src","../images/man3.PNG");
-                $(left_page).attr("id","bohyunahn");
-                $(".select_page #left_text").text("안보현");
-                left_page='#bohyunahn';
-                $(right_page).attr("src","../images/man4.PNG");
-                $(right_page).attr("id","GD");
-                $(".select_page #right_text").text("GD");
-                right_page="#GD";
-            });
+            $("#left_man").attr("src","../images/man/man3.PNG");
+            $(".select_page #left_text").text("안보현");
+            left_page='bohyunahn';
+            $("#right_man").attr("src","../images/man/man4.PNG");
+            $(".select_page #right_text").text("GD");
+            right_page="GD";
         });
     });
 
