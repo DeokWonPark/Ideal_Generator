8b50a2437714b6eca51072393fec26aad8dbde6d
20150477 <ejrdnjs96@gmail.com>
2020-05-10 02:33:32 +0900

    ADD Kakao Login file

--- 창설/front/js/login.js
+++ 창설/front/js/login.js
@@ -1,14 +1,64 @@
 $(document).ready(function(){
-    $.ajax({
-        url:'/session',
-        dataType:'json',
-        type:'POST',
-        data:{},
-        success:function(result){
-            final_path=result[0].img_path;
-            $(".select_page #Win").attr("src",result[0].img_path);
-            $(".select_page #Win").attr("id","ideal");
-            $("#ideal").attr('src',result[0].img_path);
-        }
-    })
+    // 로그인시 쿠키 처리
+    var user_cookie=$.cookie('user');
+    var index_s=user_cookie.indexOf("nickname")+11;
+    var index_e=user_cookie.indexOf("authorized")-3;
+    var user_nickname="";
+    for(var i=index_s;i<index_e;i++){
+        user_nickname+=user_cookie[i];
+    }
+    console.log(user_nickname);
+    
+    if(user_cookie){
+        $("#auth").text("Logout");
+        $("#auth").attr("href","/process/logout");
+        $("#nickname").text(user_nickname+" 님");
+    }
+    
 });
+Kakao.init('1d49add348926bb539885c56954b946a');
+console.log(Kakao.isInitialized());
+
+Kakao.API.request({
+    url: '/v1/api/talk/profile',
+    success: function(response) {
+        $("#auth").text("Logout");
+        $("#auth").attr("href","javascript:logoutWithKakao()");
+        $("#nickname").text(response.nickName+" 님");
+        $("#profile").attr("src",response.profileImageURL);
+      console.log(response);
+    },
+    fail: function(error) {
+      console.log(error);
+    }
+  });
+
+  Kakao.init('1d49add348926bb539885c56954b946a');
+      function loginWithKakao() {
+        Kakao.Auth.login({
+          success: function(authObj) {
+            alert("Kakao 회원으로 로그인합니다.");
+            location.href='/';
+          },
+          fail: function(err) {
+            alert(JSON.stringify(err))
+          },
+        })
+      }
+
+      function logoutWithKakao(){
+        if (!Kakao.Auth.getAccessToken()) {
+            console.log('Not logged in.');
+            return;
+          }
+          Kakao.Auth.logout(function() {
+            console.log(Kakao.Auth.getAccessToken());
+            alert("Kakao 회원 로그아웃");
+            location.href='/';
+          });
+      }
+
+      
+
+     
+
